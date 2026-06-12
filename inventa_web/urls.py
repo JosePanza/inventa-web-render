4b0e46be33386e11fd4a00c72f3d9bd4110c12cd
jose <jo.panza@duocuc.cl>
2026-06-12 16:08:50 -0400

    imagenes

--- inventa_web/urls.py
+++ inventa_web/urls.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Aquí le decimos al proyecto que busque las URLs de tu app core
-    path('', include('core.urls')), 
+    path('', include('core.urls')),
 ]
 
-# AQUÍ SÍ FUNCIONA: En la puerta principal del proyecto
+# En desarrollo local
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# En Render / producción simple para portafolio
+else:
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT}
+        ),
+    ]
